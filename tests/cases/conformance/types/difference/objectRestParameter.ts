84cca3b16d224ac4386a78d2d1b4a0fe5c877b77
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-01 16:13:07 -0700

    Make object rest parameter test even more complex

--- tests/cases/conformance/types/difference/objectRestParameter.ts
+++ tests/cases/conformance/types/difference/objectRestParameter.ts
@@ -4,5 +4,5 @@ function cloneAgain({ a, ...clone }: { a: number, b: string }): void {
 
 declare function suddenly(f: (a: { x: { z, ka }, y: string }) => void);
 suddenly(({ x: a, ...rest }) => rest.y);
-suddenly(({ x: { z, ...nested }, ...rest }) => rest.y + nested.ka);
+suddenly(({ x: { z = 12, ...nested }, ...rest } = { x: { z: 1, ka: 1 }, y: 'noo' }) => rest.y + nested.ka);
 
