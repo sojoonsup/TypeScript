3285cd7b848e2db76f78db8284c60eda5c6feab4
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-01 16:13:18 -0700

    Update baselines

--- tests/baselines/reference/objectRestParameter.js
+++ tests/baselines/reference/objectRestParameter.js
@@ -4,7 +4,7 @@ function cloneAgain({ a, ...clone }: { a: number, b: string }): void {
 
 declare function suddenly(f: (a: { x: { z, ka }, y: string }) => void);
 suddenly(({ x: a, ...rest }) => rest.y);
-suddenly(({ x: { z, ...nested }, ...rest }) => rest.y + nested.ka);
+suddenly(({ x: { z = 12, ...nested }, ...rest } = { x: { z: 1, ka: 1 }, y: 'noo' }) => rest.y + nested.ka);
 
 
 
@@ -22,7 +22,7 @@ suddenly((_a) => {
     var { x: a } = _a, rest = __rest(_a, ["x"]);
     return rest.y;
 });
-suddenly((_a) => {
-    var _b = _a.x, { z } = _b, nested = __rest(_b, ["z"]), rest = __rest(_a, ["x"]);
+suddenly((_a = { x: { z: 1, ka: 1 }, y: 'noo' }) => {
+    var _b = _a.x, { z = 12 } = _b, nested = __rest(_b, ["z"]), rest = __rest(_a, ["x"]);
     return rest.y + nested.ka;
 });
